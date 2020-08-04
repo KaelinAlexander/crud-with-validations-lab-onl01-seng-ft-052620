e0a19e054fe0c98c942d9fd20359f3916bdf547b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-04 16:34:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: {scope: :release_year}
-  validates :release_year, presence: true, if: :released
+  validates :release_year, presence: true, if: :is_released
   validate :check_dates
   validates :release_year, numericality: {less_than_or_equal_to: :current_year}
   validates :artist_name, presence: true
