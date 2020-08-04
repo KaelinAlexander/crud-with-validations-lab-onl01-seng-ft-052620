c1c26494b625583f5f3ad1720b5adc4643113d79
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-04 16:07:51 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,9 +4,15 @@ class Song < ApplicationRecord
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: {less_than_or_equal_to: :current_year}
   validates :artist_name, presence: true
+  vvalidate :check_dates
 
   def current_year
     Time.now.year
   end
+
+  def check_dates
+    if self.release_year && self.release_year > Date.today.year
+      self.errors.add(:release_year, "Can't be in the future")
+    e
 
 end
